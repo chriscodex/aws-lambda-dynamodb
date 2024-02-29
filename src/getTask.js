adfd0cdd4bdb91f93086a58df76d959e5484ab8d
Christian <c.espinoza.code@gmail.com>
2024-02-28 21:23:29 -0500

    table name defined for scan

--- src/getTask.js
+++ src/getTask.js
@@ -2,6 +2,10 @@ const AWS = require('aws-sdk');
 
 const getTask = async (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  await dynamodb.scan({
+    TableName: 'TaskTable',
+  }).promise()
 }
 
 module.exports = {
