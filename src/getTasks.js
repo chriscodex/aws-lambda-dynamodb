1697c1594a41c9f411014d5ea5aefe63e6450d91
Christian <c.espinoza.code@gmail.com>
2024-02-29 21:44:48 -0500

    getTasks function renamed

--- src/getTasks.js
+++ src/getTasks.js
@@ -1,20 +1,28 @@
 const AWS = require('aws-sdk');
 
-const getTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
+const getTasks = async (event) => {
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const result = await dynamodb.scan({
-    TableName: 'TaskTable',
-  }).promise()
+    const result = await dynamodb
+      .scan({
+        TableName: 'TaskTable',
+      })
+      .promise();
 
-  const tasks = result.Items
+    const tasks = result.Items;
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(tasks),
+    return {
+      status: 200,
+      body: {
+        tasks,
+      },
+    };
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
 module.exports = {
-  getTask,
-}
+  getTasks,
+};
