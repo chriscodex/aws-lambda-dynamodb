24ace6f746ced3dfa175d829ea16516643826c27
Christian <c.espinoza.code@gmail.com>
2024-03-01 16:10:51 -0500

    id extracted from event

--- src/updateTask.js
+++ src/updateTask.js
@@ -2,6 +2,9 @@ const AWS = require('aws-sdk');
 
 const updateTask = (event) => {
   const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  const { id } = event.pathParameters;
+  const { title, description } = JSON.parse(event.body);
 };
 
 module.exports = {
