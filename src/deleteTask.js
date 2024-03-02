d91c93bbc7d1108d13e2e15cdd4db7761f062e6d
Christian <c.espinoza.code@gmail.com>
2024-03-02 18:22:49 -0500

    id obtained

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,9 +1,11 @@
 const aws = require('aws-sdk');
 
-const deleteTask = async event => {
+const deleteTask = async (event) => {
   const dynamodb = new aws.DynamoDB.DocumentClient();
-}
+
+  const { id } = event.pathParameters;
+};
 
 module.exports = {
-  deleteTask
-}
+  deleteTask,
+};
