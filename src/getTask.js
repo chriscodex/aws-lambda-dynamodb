1ee0ab4b65f8d726039e878030c86dddd85ab43b
Christian <c.espinoza.code@gmail.com>
2024-03-02 16:06:19 -0500

    getTask function finished

--- src/getTask.js
+++ src/getTask.js
@@ -2,12 +2,30 @@ const AWS = require('aws-sdk');
 
 const getTask = async (event) => {
   try {
-    
-  } catch (error) {
-    
-  }
-}
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    const { id } = event.pathParameters;
+
+    const result = await dynamodb
+      .get({
+        TableName: 'TaskTable',
+        Key: {
+          id,
+        },
+      })
+      .promise();
+
+    const task = result.Item;
+
+    return {
+      status: 200,
+      body: {
+        task,
+      },
+    };
+  } catch (error) {}
+};
 
 module.exports = {
   getTask,
-}
+};
