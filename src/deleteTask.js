e6c626320baebb182a933b6ca36e7d39cdc9e4c9
Christian <c.espinoza.code@gmail.com>
2024-03-03 18:25:04 -0500

    trycatch statement added in deleteTask

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -1,25 +1,29 @@
 const aws = require('aws-sdk');
 
 const deleteTask = async (event) => {
-  const dynamodb = new aws.DynamoDB.DocumentClient();
+  try {
+    const dynamodb = new aws.DynamoDB.DocumentClient();
 
-  const { id } = event.pathParameters;
+    const { id } = event.pathParameters;
 
-  await dynamodb
-    .delete({
-      TableName: 'TaskTable',
-      Key: {
-        id,
+    await dynamodb
+      .delete({
+        TableName: 'TaskTable',
+        Key: {
+          id,
+        },
+      })
+      .promise();
+
+    return {
+      status: 200,
+      body: {
+        message: 'Task deleted successfully',
       },
-    })
-    .promise();
-
-  return {
-    status: 200,
-    body: {
-      message: 'Task deleted successfully',
-    },
-  };
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
