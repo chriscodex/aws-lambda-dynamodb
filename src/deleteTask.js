6a617a2752546bcb3db661c5c87af6b94fc21621
Christian <c.espinoza.code@gmail.com>
2024-03-03 18:23:41 -0500

    dynamo db for delete logic implemented

--- src/deleteTask.js
+++ src/deleteTask.js
@@ -4,6 +4,15 @@ const deleteTask = async (event) => {
   const dynamodb = new aws.DynamoDB.DocumentClient();
 
   const { id } = event.pathParameters;
+
+  await dynamodb
+    .delete({
+      TableName: 'TaskTable',
+      Key: {
+        id,
+      },
+    })
+    .promise();
 };
 
 module.exports = {
