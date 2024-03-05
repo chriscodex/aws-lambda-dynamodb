60c048ee85a10465db7b7ff6e629f287ff2f17bc
Christian <c.espinoza.code@gmail.com>
2024-03-04 21:32:54 -0500

    trycatch statement implemented in updateTask

--- src/updateTask.js
+++ src/updateTask.js
@@ -1,32 +1,36 @@
 const AWS = require('aws-sdk');
 
 const updateTask = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const { id } = event.pathParameters;
-  const { done, title, description } = JSON.parse(event.body);
+    const { id } = event.pathParameters;
+    const { done, title, description } = JSON.parse(event.body);
 
-  await dynamodb
-    .update({
-      TableName: 'TaskTable',
-      Key: {
-        id,
-      },
-      UpdateExpression:
-        'set done = :done, title = :title, description = :description',
-      ExpressionAttributeValues: {
-        ':done': done,
-        ':title': title,
-        ':description': description,
-      },
-      ReturnValues: 'ALL_NEW',
-    })
-    .promise();
+    await dynamodb
+      .update({
+        TableName: 'TaskTable',
+        Key: {
+          id,
+        },
+        UpdateExpression:
+          'set done = :done, title = :title, description = :description',
+        ExpressionAttributeValues: {
+          ':done': done,
+          ':title': title,
+          ':description': description,
+        },
+        ReturnValues: 'ALL_NEW',
+      })
+      .promise();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Task updated successfully' }),
-  };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Task updated successfully' }),
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
